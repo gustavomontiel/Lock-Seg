1a32aeb92061e6506b98f9c38340b2b5144dd0ce
cris <cris.hol47@gmail.com>
2019-11-11 20:12:15 -0300

    APP - servicio getURL

--- app/src/app/services/urls.service.ts
+++ app/src/app/services/urls.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { from } from 'rxjs';
+import { map, filter, mergeMap } from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +15,41 @@ export class UrlsService {
     private storage: Storage
   ) {  }
 
+  
+  private getAuthHeaders() {
+    return from(this.storage.get('USER_INFO').data.token);
+  }
+
+
 
   getUrl(nombre: string) {
 
-    const url = 'http://lock-api.grupo-sim.com.ar/parametros/'+nombre;
-
-    return this.http.get(url).pipe(
-      map((respuesta: any) => {
-        return respuesta;
-      }),
-      
+    var responseStream = this.getAuthHeaders().pipe(
+      mergeMap( userinfo => {
+      console.log(userinfo);
+      })
     );
+
+      /*console.log(response.data.token);
+
+
+
+      const headers = new HttpHeaders({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': response.data.token
+      });
+
+      const url = 'http://lock-api.grupo-sim.com.ar/parametros/'+nombre;
+
+      return this.http.get(url, { headers: headers }).pipe(
+        map((respuesta: any) => {
+          return respuesta;
+        }),
+        
+      );*/
+ 
+
+    
   }
 }
