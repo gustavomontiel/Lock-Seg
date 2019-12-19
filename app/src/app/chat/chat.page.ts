6a69857d36c36a8de75c96eb15615057441e4a96
cris <cris.hol47@gmail.com>
2019-12-19 14:38:34 -0300

    APP - mas nuevo y chat

--- app/src/app/chat/chat.page.ts
+++ app/src/app/chat/chat.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UrlsService } from '../services/urls.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { InAppBrowser , InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
+
 
 
 @Component({
@@ -12,21 +14,40 @@ export class ChatPage implements OnInit {
 
   urlChat: any;
   parametro: any;
+  nroCli: any;
+  elemento: any;
+  mio: any;
 
+  
   constructor(
     private urlsService: UrlsService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private theInAppBrowser: InAppBrowser,
   ) { }
 
   ngOnInit() {
     this.getUrlChat();
   }
 
+  /*ngAfterViewChecked() {
+    this.putNroCli();
+  }*/
+
+  private putNroCli(mio) {
+    /*
+    console.log('mio'+mio);
+    this.nroCli = 4565;
+    this.elemento = document.getElementById('prechat0Field');
+    console.log(this.elemento);
+    document.getElementById('prechat0Field').nodeValue = '55';
+*/
+  }
   private getUrlChat() {
     console.log('Chat');
     this.urlsService.getUrl('chat')
       .subscribe(parametro => {
         this.urlChat = this.sanitizer.bypassSecurityTrustResourceUrl(parametro.data.valor);
+        
       });
   }
 
