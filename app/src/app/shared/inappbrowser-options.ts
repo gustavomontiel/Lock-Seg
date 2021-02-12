b4d01931f3ed67010b5481c22a6d4f722dc05ce5
Gustavo Montiel <gustavo.montiel@indusoftware.com.ar>
2021-02-11 21:00:00 -0300

    app - color negro en el toolbar del inappbrowser

--- app/src/app/shared/inappbrowser-options.ts
+++ app/src/app/shared/inappbrowser-options.ts
@@ -1,4 +1,4 @@
 // export const inappbrowserOption = 'location=no,toolbarcolor=#000000,footer=yes,footercolor=#000000,closebuttoncaption=Cerrar,hardwareback=yes';
-export const inappbrowserOption = 'location=yes,hideurlbar=yes,footer=no,closebuttoncaption=Guazú seguridad   -   Volver,hardwareback=yes'
+export const inappbrowserOption = 'location=yes,hideurlbar=yes,footer=no,toolbarcolor=#000000,hardwareback=yes,closebuttoncaption=Guazú seguridad   -   Volver'
 // export const inappbrowserOptionSistem = 'location=no,toolbarcolor=#000000,footer=yes,footercolor=#000000,closebuttoncaption=Cerrar,hardwareback=yes';
 export const inappbrowserOptionSistem = '';
