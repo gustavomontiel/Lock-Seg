b73f23ed0eead7ae4b8c86d5e5d4d0f596512730
gustavomontiel <gustavomontiel@gmail.com>
2021-10-31 20:15:00 -0300

    web - agregar cuentas Deitres a usuarios

--- web/src/app/auth/auth.service.ts
+++ web/src/app/auth/auth.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import Swal from 'sweetalert2';
-import { map, catchError } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-import { User } from './user.model';
-import { AppState } from '../app.reducer';
-import { Store } from '@ngrx/store';
-import { Subscription, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { ActivarLoadingAction, DesactivarLoadingAction } from './../shared/ui.actions';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import Swal from "sweetalert2";
+import { map, catchError, tap } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { User } from "./user.model";
+import { AppState } from "../app.reducer";
+import { Store } from "@ngrx/store";
+import { Subscription, throwError } from "rxjs";
+import { environment } from "src/environments/environment";
+import {
+  ActivarLoadingAction,
+  DesactivarLoadingAction,
+} from "./../shared/ui.actions";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
   private userSubscrition: Subscription = new Subscription();
 
   private usuario: User;
@@ -25,25 +27,23 @@ export class AuthService {
     private router: Router,
     private store: Store<AppState>
   ) {
-    this.token = sessionStorage.getItem('token') || '';
-    this.usuario = JSON.parse(sessionStorage.getItem('usuario')) || null;
+    this.token = sessionStorage.getItem("token") || "";
+    this.usuario = JSON.parse(sessionStorage.getItem("usuario")) || null;
   }
 
   crearUsuario(user: User) {
-
     this.store.dispatch(new ActivarLoadingAction());
 
-    const url = environment.APIEndpoint + '/users';
+    const url = environment.APIEndpoint + "/users";
 
-    return this.http.post(url, user)
-    .pipe(
+    return this.http.post(url, user).pipe(
       map((resp: any) => {
         this.store.dispatch(new DesactivarLoadingAction());
-        console.log('resp serv:', resp);
+        console.log("resp serv:", resp);
         return resp;
       }),
-      catchError(err => {
-        console.log('error serv:', err);
+      catchError((err) => {
+        console.log("error serv:", err);
         this.store.dispatch(new DesactivarLoadingAction());
         return throwError(err);
       })
@@ -51,64 +51,56 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-
     this.store.dispatch(new ActivarLoadingAction());
 
-    const url = environment.APIEndpoint + '/auth/login';
+    const url = environment.APIEndpoint + "/auth/login";
     const usuario = {
       email,
-      password
+      password,
     };
 
-    this.http.post(url, usuario)
-      .subscribe(
-        (resp: any) => {
-          this.usuario = resp.data.user;
-          this.token = resp.data.token;
-          sessionStorage.setItem('token', resp.data.token);
-          sessionStorage.setItem('usuario', JSON.stringify(resp.data.user));
-          this.store.dispatch(new DesactivarLoadingAction());
-          this.router.navigate(['/']);
-        },
-        (error: any) => {
-          this.store.dispatch(new DesactivarLoadingAction());
-          Swal.fire('Error en el login', error.message, 'error');
-        }
-      );
+    this.http.post(url, usuario).subscribe(
+      (resp: any) => {
+        this.usuario = resp.data.user;
+        this.token = resp.data.token;
+        sessionStorage.setItem("token", resp.data.token);
+        sessionStorage.setItem("usuario", JSON.stringify(resp.data.user));
+        this.store.dispatch(new DesactivarLoadingAction());
+        this.router.navigate(["/"]);
+      },
+      (error: any) => {
+        this.store.dispatch(new DesactivarLoadingAction());
+        Swal.fire("Error en el login", error.message, "error");
+      }
+    );
   }
-
 
   logout() {
-
     this.usuario = null;
-    this.token = '';
+    this.token = "";
 
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('usuario');
-    this.router.navigate(['/login']);
-
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("usuario");
+    this.router.navigate(["/login"]);
   }
-
 
   isAuth() {
-    return (this.token.length > 5) ? true : false;
+    return this.token.length > 5 ? true : false;
   }
-
 
   getUsuario() {
     return { ...this.usuario };
   }
 
-
   getUsuarioId(id: string) {
     Swal.fire({
-      text: 'Buscando Datos',
+      text: "Buscando Datos",
       onBeforeOpen: () => {
         Swal.showLoading();
-      }
+      },
     });
 
-    const url = environment.APIEndpoint + '/users/' + id;
+    const url = environment.APIEndpoint + "/users/" + id;
     return this.http.get(url).pipe(
       map((resp: any) => {
         Swal.close();
@@ -117,16 +109,15 @@ export class AuthService {
     );
   }
 
-
   getUsuarios() {
     Swal.fire({
-      text: 'Buscando usuarios',
+      text: "Buscando usuarios",
       onBeforeOpen: () => {
         Swal.showLoading();
-      }
+      },
     });
 
-    const url = environment.APIEndpoint + '/users';
+    const url = environment.APIEndpoint + "/users";
 
     return this.http.get(url).pipe(
       map((respuesta: any) => {
@@ -137,7 +128,9 @@ export class AuthService {
   }
 
   isAdmin() {
-    return this.usuario ? this.usuario.roleNames.includes('administrador') : false;
+    return this.usuario
+      ? this.usuario.roleNames.includes("administrador")
+      : false;
   }
 
   getRoles() {
@@ -145,79 +138,103 @@ export class AuthService {
   }
 
   updateUser(user: User) {
-
     if (user.id) {
-
       this.store.dispatch(new ActivarLoadingAction());
 
-      let url = environment.APIEndpoint + '/users';
-      url += '/' + user.id;
+      let url = environment.APIEndpoint + "/users";
+      url += "/" + user.id;
 
-      return this.http.put(url, user)
-      .pipe(
+      return this.http.put(url, user).pipe(
         map((resp: any) => {
           this.store.dispatch(new DesactivarLoadingAction());
           return resp;
         }),
-        catchError(err => {
-          console.log('Error:', err);
+        catchError((err) => {
+          console.log("Error:", err);
           this.store.dispatch(new DesactivarLoadingAction());
           return throwError(err);
         })
       );
     } else {
-      console.log('no se puede actualizar un objeto sin id');
-
+      console.log("no se puede actualizar un objeto sin id");
     }
-
   }
 
   deleteUser(user: User) {
-
     Swal.fire({
-      text: 'Procesando solicitud',
+      text: "Procesando solicitud",
       onBeforeOpen: () => {
         Swal.showLoading();
-      }
+      },
     });
 
-    const url = environment.APIEndpoint + '/users/' + user.id;
-    return this.http.delete(url)
-      .pipe(
-        map((resp: any) => {
-          Swal.close();
-          return resp;
-        })
-      );
-
+    const url = environment.APIEndpoint + "/users/" + user.id;
+    return this.http.delete(url).pipe(
+      map((resp: any) => {
+        Swal.close();
+        return resp;
+      })
+    );
   }
 
-  cambiarPassword( id: number, pass: any ) {
-
-    if ( pass ) {
-
+  cambiarPassword(id: number, pass: any) {
+    if (pass) {
       this.store.dispatch(new ActivarLoadingAction());
 
-      let url = environment.APIEndpoint + '/actualizar-password';
-      url += '/' + id;
+      let url = environment.APIEndpoint + "/actualizar-password";
+      url += "/" + id;
 
-      return this.http.put(url, pass)
-      .pipe(
+      return this.http.put(url, pass).pipe(
         map((resp: any) => {
           this.store.dispatch(new DesactivarLoadingAction());
           return resp;
         }),
-        catchError(err => {
-          console.log('Error:', err);
+        catchError((err) => {
+          console.log("Error:", err);
           this.store.dispatch(new DesactivarLoadingAction());
           return throwError(err);
         })
       );
     } else {
-      console.log('no se puede actualizar la contraseña');
-
+      console.log("no se puede actualizar la contraseña");
     }
-
   }
+
+  getUserDeitresAccounts( userId: string ) {
+    const url = environment.APIEndpoint + "/cuentas/user/" + userId;
+    return this.http.get(url).pipe(
+      tap((resp: any) => {
+        Swal.close();
+      })
+    );
+  }
+
+  deleteUserDeitresAccount( id: string ) {
+    const url = environment.APIEndpoint + "/cuentas/" + id;
+    return this.http.delete(url).pipe(
+      tap((resp: any) => {
+        Swal.close();
+      })
+    );
+  }
+
+  updateUserDeitresAccount( account: any ) {
+    const url = environment.APIEndpoint + "/cuentas/" + account.id;
+    return this.http.put(url, account).pipe(
+      tap((resp: any) => {
+        Swal.close();
+      })
+    );
+  }
+
+  createUserDeitresAccount( account: any ) {
+    const url = environment.APIEndpoint + "/cuentas";
+    return this.http.post(url, account).pipe(
+      tap((resp: any) => {
+        Swal.close();
+      })
+    );
+  }
+
 
 }
--- web/src/app/dashboard/usuario/editar-usuario/editar-usuario.component.ts
+++ web/src/app/dashboard/usuario/editar-usuario/editar-usuario.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import Swal from 'sweetalert2';
-import { Subscription } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/app/app.reducer';
-import { User } from 'src/app/auth/user.model';
-import { AuthService } from 'src/app/auth/auth.service';
-import { roleNames } from 'src/app/shared/guards/roleNames.data';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import Swal from "sweetalert2";
+import { Subscription } from "rxjs";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/app.reducer";
+import { User } from "src/app/auth/user.model";
+import { AuthService } from "src/app/auth/auth.service";
+import { roleNames } from "src/app/shared/guards/roleNames.data";
+import { HttpErrorResponse } from "@angular/common/http";
+import { MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 
 @Component({
-  selector: 'app-editar-usuario',
-  templateUrl: './editar-usuario.component.html',
-  styleUrls: ['./editar-usuario.component.scss']
+  selector: "app-editar-usuario",
+  templateUrl: "./editar-usuario.component.html",
+  styleUrls: ["./editar-usuario.component.scss"],
 })
 export class EditarUsuarioComponent implements OnInit, OnDestroy {
-
   cargando: boolean;
   subscription: Subscription;
 
@@ -26,35 +26,48 @@ export class EditarUsuarioComponent implements OnInit, OnDestroy {
 
   formPassword: FormGroup;
 
+  formDeitresAccount: FormGroup;
+  deitresAccounts: any[] = [];
+
+  dataSource: any;
+  displayedColumns: string[] = ['account', 'descripcion', 'acciones'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
   constructor(
     public authService: AuthService,
     public router: Router,
     public activatedRoute: ActivatedRoute,
     public store: Store<AppState>
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
-
-    this.subscription = this.store.select('ui').subscribe(ui => {
+    this.subscription = this.store.select("ui").subscribe((ui) => {
       this.cargando = ui.isLoading;
     });
 
     this.formUsuario = new FormGroup({
       nombre: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      telefono: new FormControl('', Validators.required),
+      telefono: new FormControl("", Validators.required),
       roleNames: new FormControl([], [Validators.required]),
-      codigo_gestion: new FormControl('', Validators.required),
+      codigo_gestion: new FormControl("", Validators.required),
     });
 
     this.formPassword = new FormGroup({
-      password_nuevo: new FormControl(null, Validators.required)
+      password_nuevo: new FormControl(null, Validators.required),
     });
 
-    this.activatedRoute.params.subscribe(params => {
+    this.formDeitresAccount = new FormGroup({
+      id: new FormControl(null),
+      account: new FormControl(null, Validators.required),
+      descripcion: new FormControl(null, Validators.required)
+    });
+
+    this.activatedRoute.params.subscribe((params) => {
       const id = params.id;
       this.cargarUsuario(id);
+      this.getDeitresAccounts(id);
     });
   }
 
@@ -63,55 +76,58 @@ export class EditarUsuarioComponent implements OnInit, OnDestroy {
   }
 
   cargarUsuario(id: string) {
-
-    this.authService.getUsuarioId(id)
-      .subscribe(usuario => {
-        this.usuario = usuario.data;
-        console.log(this.usuario);
-        this.formUsuario.setValue({
-          nombre: this.usuario.nombre,
-          email: this.usuario.email,
-          telefono: this.usuario.telefono,
-          roleNames: this.usuario.roleNames ? this.usuario.roleNames : '',
-          codigo_gestion: this.usuario.codigo_gestion
-        });
+    this.authService.getUsuarioId(id).subscribe((usuario) => {
+      this.usuario = usuario.data;
+      this.formUsuario.setValue({
+        nombre: this.usuario.nombre,
+        email: this.usuario.email,
+        telefono: this.usuario.telefono,
+        roleNames: this.usuario.roleNames ? this.usuario.roleNames : "",
+        codigo_gestion: this.usuario.codigo_gestion,
       });
+    });
+  }
 
+  getDeitresAccounts(userId: string) {
+    this.authService.getUserDeitresAccounts(userId).subscribe((deitresAccounts) => {
+      if (deitresAccounts.error === 'false') {
+        this.deitresAccounts = deitresAccounts.data.cuentas;
+        this.dataSource = new MatTableDataSource(this.deitresAccounts);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    });
   }
 
   actualizarUsuario() {
-
     Swal.fire({
-      title: 'Guardar cambios?',
-      text: 'Confirma los cambios?',
-      type: 'question',
+      title: "Guardar cambios?",
+      text: "Confirma los cambios?",
+      type: "question",
       showCancelButton: true,
     }).then((result) => {
-
       if (result.value) {
+        const user = { ...this.formUsuario.value, id: this.usuario.id };
 
-        const user = { ... this.formUsuario.value, id: this.usuario.id };
-        console.log(user);
-
-        this.authService.updateUser( user ).subscribe(
-          resp => {
+        this.authService.updateUser(user).subscribe(
+          (resp) => {
             Swal.fire(
-              'Guardado!',
-              'Los cambios fueron guardados correctamente.',
-              'success'
+              "Guardado!",
+              "Los cambios fueron guardados correctamente.",
+              "success"
             );
           },
-          error => {
-            let msg = '';
+          (error) => {
+            let msg = "";
             if (error instanceof HttpErrorResponse) {
               const validationErrors = error.error.data;
-              if ( error.status === 422 || error.status === 400) {
-                Object.keys(validationErrors).forEach(prop => {
+              if (error.status === 422 || error.status === 400) {
+                Object.keys(validationErrors).forEach((prop) => {
                   const formControl = this.formUsuario.get(prop);
                   if (formControl) {
-                    msg += '<br>' + validationErrors[prop];
+                    msg += "<br>" + validationErrors[prop];
                     formControl.setErrors({
-                      serverError: validationErrors[prop]
+                      serverError: validationErrors[prop],
                     });
                   }
                 });
@@ -119,50 +135,45 @@ export class EditarUsuarioComponent implements OnInit, OnDestroy {
             }
 
             Swal.fire(
-              'Error!',
-              'Los cambios no fueron guardados.' + msg,
-              'error'
+              "Error!",
+              "Los cambios no fueron guardados." + msg,
+              "error"
             );
-
           }
         );
       }
     });
-
   }
 
   actualizarPassword() {
-
     Swal.fire({
-      title: 'Combiar contraseña?',
-      text: 'Confirma el cambio de contraseña?',
-      type: 'question',
+      title: "Combiar contraseña?",
+      text: "Confirma el cambio de contraseña?",
+      type: "question",
       showCancelButton: true,
     }).then((result) => {
-
       if (result.value) {
-
         const pass = this.formPassword.value;
 
-        this.authService.cambiarPassword( this.usuario.id, pass ).subscribe(
-          resp => {
+        this.authService.cambiarPassword(this.usuario.id, pass).subscribe(
+          (resp) => {
             Swal.fire(
-              'Guardado!',
-              'La contraseña ha sido actualizada correctamente.',
-              'success'
+              "Guardado!",
+              "La contraseña ha sido actualizada correctamente.",
+              "success"
             );
           },
-          error => {
-            let msg = '';
+          (error) => {
+            let msg = "";
             if (error instanceof HttpErrorResponse) {
               const validationErrors = error.error.data;
-              if ( error.status === 422 || error.status === 400) {
-                Object.keys(validationErrors).forEach(prop => {
+              if (error.status === 422 || error.status === 400) {
+                Object.keys(validationErrors).forEach((prop) => {
                   const formControl = this.formUsuario.get(prop);
                   if (formControl) {
-                    msg += '<br>' + validationErrors[prop];
+                    msg += "<br>" + validationErrors[prop];
                     formControl.setErrors({
-                      serverError: validationErrors[prop]
+                      serverError: validationErrors[prop],
                     });
                   }
                 });
@@ -170,16 +181,165 @@ export class EditarUsuarioComponent implements OnInit, OnDestroy {
             }
 
             Swal.fire(
-              'Error!',
-              'Los cambios no fueron guardados.' + msg,
-              'error'
+              "Error!",
+              "Los cambios no fueron guardados." + msg,
+              "error"
             );
-
           }
         );
       }
     });
+  }
 
+  grabarDeitresAccount() {
+    Swal.fire({
+      title: "Guardar cambios?",
+      text: "Confirma los cambios?",
+      type: "question",
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.value) {
+        const account = this.formDeitresAccount.value;
+        account.user_id = this.usuario.id;
+        if ( account.id ) {
+          this.authService.updateUserDeitresAccount(account).subscribe(
+            (resp) => {
+              Swal.fire(
+                "Guardado!",
+                "Datos actualizados correctamente.",
+                "success"
+              ).then(
+                res => {
+                  this.setFormDeitresAccount(null, null, null);
+                  this.getDeitresAccounts(this.usuario.id.toString());
+                }
+              )
+            },
+            (error) => {
+              let msg = "";
+              if (error instanceof HttpErrorResponse) {
+                const validationErrors = error.error.data;
+                if (error.status === 422 || error.status === 400) {
+                  Object.keys(validationErrors).forEach((prop) => {
+                    const formControl = this.formDeitresAccount.get(prop);
+                    if (formControl) {
+                      msg += "<br>" + validationErrors[prop];
+                      formControl.setErrors({
+                        serverError: validationErrors[prop],
+                      });
+                    }
+                  });
+                }
+              }
+              Swal.fire(
+                "Error!",
+                "Los cambios no fueron guardados." + msg,
+                "error"
+              );
+            }
+          )
+        } else {
+          this.authService.createUserDeitresAccount(account).subscribe(
+            (resp) => {
+              Swal.fire(
+                "Guardado!",
+                "Datos actualizados correctamente.",
+                "success"
+              ).then(
+                res => {
+                  this.setFormDeitresAccount(null, null, null);
+                  this.getDeitresAccounts(this.usuario.id.toString());
+                }
+              )
+            },
+            (error) => {
+              let msg = "";
+              if (error instanceof HttpErrorResponse) {
+                const validationErrors = error.error.data;
+                if (error.status === 422 || error.status === 400) {
+                  Object.keys(validationErrors).forEach((prop) => {
+                    const formControl = this.formDeitresAccount.get(prop);
+                    if (formControl) {
+                      msg += "<br>" + validationErrors[prop];
+                      formControl.setErrors({
+                        serverError: validationErrors[prop],
+                      });
+                    }
+                  });
+                }
+              }
+              Swal.fire(
+                "Error!",
+                "Los cambios no fueron guardados." + msg,
+                "error"
+              );
+            }
+          )
+        }
+      }
+    });
+  }
+
+  cancelarFormDeitresAccount() {
+    this.setFormDeitresAccount(null, null, null);
+    this.formDeitresAccount.markAsPristine();
+  }
+
+  setFormDeitresAccount(id, account, descripcion) {
+    this.formDeitresAccount.setValue( { id: id, account: account, descripcion: descripcion } );
+  }
+
+  editarFormDeitresAccount(account) {
+    this.setFormDeitresAccount( account.id, account.account, account.descripcion );
+    this.formDeitresAccount.markAsDirty();
+  }
+
+  eliminarFormDeitresAccount(account) {
+    const msg = 'Confirma eliminar el registro: "' + account.descripcion + '"?';
+    Swal.fire({
+      title: "Guardar cambios?",
+      text: msg,
+      type: "question",
+      showCancelButton: true,
+    }).then((result) => {
+      if (result.value) {
+        this.authService.deleteUserDeitresAccount(account.id).subscribe(
+          (resp) => {
+            Swal.fire(
+              "Guardado!",
+              "Datos eliminado correctamente.",
+              "success"
+            ).then(
+              res => {
+                this.getDeitresAccounts(this.usuario.id.toString());
+              }
+            )
+          },
+          (error) => {
+            let msg = "";
+            if (error instanceof HttpErrorResponse) {
+              const validationErrors = error.error.data;
+              if (error.status === 422 || error.status === 400) {
+                Object.keys(validationErrors).forEach((prop) => {
+                  const formControl = this.formDeitresAccount.get(prop);
+                  if (formControl) {
+                    msg += "<br>" + validationErrors[prop];
+                    formControl.setErrors({
+                      serverError: validationErrors[prop],
+                    });
+                  }
+                });
+              }
+            }
+            Swal.fire(
+              "Error!",
+              "Los cambios no fueron guardados." + msg,
+              "error"
+            );
+          }
+        )
+      }
+    })
   }
 
 }
