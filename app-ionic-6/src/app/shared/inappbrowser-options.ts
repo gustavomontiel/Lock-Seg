62e3ed39170f56cec2a384fbf48d56a704513f19
gustavomontiel <gustavomontiel@gmail.com>
2021-11-19 19:57:30 -0300

    app - ajustes solicitados

--- app-ionic-6/src/app/shared/inappbrowser-options.ts
+++ app-ionic-6/src/app/shared/inappbrowser-options.ts
@@ -1,4 +1,4 @@
 export const inappbrowserOptionDefault = '';
 export const inappbrowserOptionIos = 'location=no,toolbarcolor=#000000,footer=yes,footercolor=#000000,closebuttoncaption=Cerrar,hardwareback=yes';
-export const inappbrowserOptionAndroid = 'location=yes,hideurlbar=yes,footer=no,toolbarcolor=#000000,hardwareback=yes,closebuttoncaption=Guazú seguridad   -   Volver';
+export const inappbrowserOptionAndroid = 'location=yes,hideurlbar=yes,footer=no,toolbarcolor=#000000,hardwareback=yes,closebuttoncaption=Guazú Seguridad   -   Volver';
 export const inappbrowserOptionSistem = '';
