46ce108a11c5ebb58852d359b93c8489dfba1b96
Vholy <lnescalada@gmail.com>
2023-08-17 19:18:46 -0300

    Boton de Atras

--- app-ionic-6/src/app/app.component.ts
+++ app-ionic-6/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -18,27 +18,40 @@ export class AppComponent {
     private statusBar: StatusBar,
     private router: Router,
     public activatedRoute: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private navCtrl: NavController
   ) {
-    
+
     this.initializeApp();
   }
 
+  ngOnInit() {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      if (window.location.pathname === '/home') {
+        // Salir de la aplicación si estamos en la página de inicio ('/home')
+        navigator['app'].exitApp();
+      } else {
+        // Navegar hacia atrás si no estamos en la página de inicio
+        this.navCtrl.pop();
+      }
+    });
+  }
+
   async initializeApp() {
-    
+
     this.platform.ready().then(async () => {
 
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
       this.authService.authState.subscribe(state => {
-        
+
         let ruta: string = this.activatedRoute.snapshot['_routerState'].url;
-         
+
         ruta = ( ruta !== '' && !ruta.includes('login') && !!ruta ) ? ruta : 'home';
 
         state ? this.router.navigate([ruta], { replaceUrl: true }) : this.router.navigate(['login'], { skipLocationChange: true });
-        
+
       });
 
     });
